29d9e02ce31e179f926ba860b0429d67d7a82f88
Ai-Lyn Tang <ai-lyntang@192-168-1-4.tpgi.com.au>
2017-06-13 18:58:15 +1000

    Setup new project

---
+++ ReviewsFetcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 13/6/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
