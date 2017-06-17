22f0b91afd958e33fb92500411ac27e75e5a0f6c
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-06-18 09:19:40 +1000

    [home-screen] Create IBoutlets in `HomeTableViewCell.swift`

---
+++ Source/Home/HomeTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeTableViewCell.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 18/6/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+  
+  //Properties
+  @IBOutlet weak private var appImageView: UIImageView!
+  @IBOutlet weak private var appNameLabel: UILabel!
+  @IBOutlet weak private var appleAppStoreStarRatingLabel: UILabel!
+  @IBOutlet weak private var googlePlayStoreStarRatingLabel: UILabel!
+  
+}
