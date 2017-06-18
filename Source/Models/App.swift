379b14bf82281f1c383af780c0444a467e3eb08c
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-06-18 17:46:58 +1000

    [home-screen] Create `App` model to use in the home table view

---
+++ Source/Models/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 18/6/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
+
+struct App {
+  
+  //TODO: Figure out how to uncomment appImage as UIImageView is not a type
+//  let appImage: UIImageView
+  let appName: String
+  let appleAppStoreStarRating: String
+  let googlePlayStoreStarRating: String
+
+}
