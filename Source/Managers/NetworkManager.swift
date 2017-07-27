233c18db75fbd488d8630e808520701890e60756
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-07-28 07:17:05 +1000

    [FF-1] Update completion handler in `fetchApp` method to receive an app

--- Source/Managers/NetworkManager.swift
+++ Source/Managers/NetworkManager.swift
@@ -18,10 +18,12 @@ final class NetworkManager {
   /**
    This function takes an `appId` that correlates to the product id used by App Figures.
 
-   - parameter appId: the App Figures product id unique to every app
+   - parameter appId: an integer with the App Figures product id unique to every app
    */
-  class func fetchApp(appId: Int, completionHandler: @escaping (Void) -> Void) {
+  class func fetchApp(appId: Int, completionHandler: @escaping (App?) -> Void) {
 
+    // TODO: I think there's an authentication issue here
+    // So it always returns no app, even though in the browser the same URL returns an app
     let clientKey = "6cc908cce44e492f844fd7921b953878"
     let urlStringStart = "https://api.appfigures.com/v2/products/"
     let urlStringEnd = "?client_key="
@@ -30,13 +32,16 @@ final class NetworkManager {
 
     Alamofire.request(urlString).responseData{ dataResponse in
 
+      var app = App()
+      
       if let data = dataResponse.result.value {
-        let json = JSON(data: data)
-        let arrayOfApps = ["23432"]
-        print("Woo hoo! \(arrayOfApps)")
 
-        completionHandler()
+        let json = JSON(data: data)
         app = App.parseAppOverview(from: json)
+        completionHandler(app)
+
+      } else {
+        print("No data returned from network call")
       }
     }
   }
