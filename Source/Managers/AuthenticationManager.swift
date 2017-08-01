ed160e043beec8be5665085d60fcb83e79d226d0
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-02 07:06:23 +1000

    [FF-1] Add `AuthenticationManager.swift` for OAuth1

---
+++ Source/Managers/AuthenticationManager.swift
@@ -0,0 +1,103 @@
+//
+//  AuthenticationManager.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 31/7/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+
+final class AuthenticationManager {
+
+  // MARK: Public Method
+
+  class func authenticateWithoutAuthorisation() {
+
+    // create an instance and retain it
+    let oauthswift = OAuth1Swift(
+      consumerKey:    "6cc908cce44e492f844fd7921b953878",
+      consumerSecret: "a72e958e98f74042adcabcc9ef0a88cd&",
+      requestTokenUrl: "https://api.appfigures.com/v2/oauth/request_token",
+      authorizeUrl:    "https://api.appfigures.com/v2/oauth/authorize",
+      accessTokenUrl:  "https://api.appfigures.com/v2/oauth/access_token"
+    )
+
+    // authorize
+    // Callback URLs tried
+//"oauth-swift://oauth-callback/appfigures"
+    // "oob" generates request error -11
+
+    let url1 = "https://api.appfigures.com/v2/products/40332083066?"
+    let url2 = "oauth-swift://oauth-callback/reviewsFetcher"
+
+    let handle = oauthswift.authorize(
+      withCallbackURL: URL(string: url2)!,
+      success: { credential, response, parameters in
+        print(credential.oauthToken)
+        print(credential.oauthTokenSecret)
+        print(parameters["user_id"])
+        // Do your request
+    },
+      failure: { error in
+        print(error.localizedDescription)
+    }
+    )
+  }
+
+  // Builds but I can't figure out the callback URL, so it neither succeeds nor fails
+  class func authenticateWithAuthorisation() {
+
+    // create an instance and retain it
+    let oauthswift = OAuth1Swift(
+      consumerKey:    "6cc908cce44e492f844fd7921b953878",
+      consumerSecret: "a72e958e98f74042adcabcc9ef0a88cd",
+      requestTokenUrl: "https://api.appfigures.com/v2/oauth/request_token",
+      authorizeUrl:    "https://api.appfigures.com/v2/oauth/authorize",
+      accessTokenUrl:  "https://api.appfigures.com/v2/oauth/access_token"
+    )
+
+    // authorize
+    let handle = oauthswift.authorize(
+//      withCallbackURL: URL(string: "oauth-swift://oauth-callback/twitter")!,
+      withCallbackURL: URL(string: "https://api.appfigures.com/v2")!,
+      success: { credential, response, parameters in
+        print(credential.oauthToken)
+        print(credential.oauthTokenSecret)
+        print(parameters["user_id"])
+        // Do your request
+    },
+      failure: { error in
+        print(error.localizedDescription)
+    }
+    )
+  }
+
+  // Builds but always fails as access token is invalid
+  // I have tried with the & and without the & at the end of the consumer secret
+  class func authenticateWithoutAuthorisation2() {
+
+    // create an instance and retain it
+    let oauthswift = OAuth1Swift(
+      consumerKey:    "6cc908cce44e492f844fd7921b953878",
+      consumerSecret: "a72e958e98f74042adcabcc9ef0a88cd&",
+      requestTokenUrl: "https://api.appfigures.com/v2/oauth/request_token",
+      authorizeUrl:    "https://api.appfigures.com/v2/oauth/authorize",
+      accessTokenUrl:  "https://api.appfigures.com/v2/oauth/access_token"
+    )
+
+    // do your HTTP request without authorize
+    oauthswift.client.get("https://api.appfigures.com/v2/products/40332083066?",
+                          success: { response in
+                            //....
+                            print("Authentication success!")
+    },
+                          failure: { error in
+                            print("Authentication failure")
+                            print(error)
+    }
+    )
+  }
+
+}
