1370f0c66b544db6638ef6d7ebd8c242f7bbe27f
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-03 07:20:14 +1000

    [FF-1] Add placeholder file `StubAppOverview` for AppFigures json output

---
+++ Source/Supporting Files/StubAppOverview.swift
@@ -0,0 +1,13 @@
+//
+//  StubAppOverview.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 3/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
+
+final class StubAppOverview {
+
+}
