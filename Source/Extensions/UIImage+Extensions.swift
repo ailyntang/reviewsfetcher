dc52100d8314315e084264a0efaf7e61781f59c9
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-05 16:11:18 +1000

    [FF-1] Create new placeholder file for `UIImage+Extensions`

---
+++ Source/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extensions.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 5/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+
+}
