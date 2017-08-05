0c31dbafa8f247ad3f225963ea8e1d7945561528
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-05 16:27:12 +1000

    [FF-1] Add `fromUrlString` method to convert a string into a `UIImage`

--- Source/Extensions/UIImage+Extensions.swift
+++ Source/Extensions/UIImage+Extensions.swift
@@ -10,4 +10,27 @@ import UIKit
 
 extension UIImage {
 
+  /**
+   Takes a string and returns a UIImage.
+   
+   - parameter string: A string containing a url address that is an image
+   - returns: A UIImage based on the url string or a default image if the url string
+   cannot be converted to an image.
+  */
+  func fromUrlString(string: String) -> UIImage {
+
+    var image = #imageLiteral(resourceName: "imgDefaultApp")
+
+    if let url = URL(string: string) {
+      DispatchQueue.global(qos: .background).async {
+        if let data = try? Data(contentsOf: url, options: NSData.ReadingOptions.uncached) {
+          DispatchQueue.main.async {
+            image = UIImage(data: data)!
+          }
+        }
+      }
+    }
+    return image
+  }
+
 }
