d78edbc2684bed955d5d393fb21d8ad2148dc769
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-08 19:59:44 +1000

    [FF-1] Parse `releaseDate` and `updatedDate` from app overview json

--- Source/Managers/ParseManager.swift
+++ Source/Managers/ParseManager.swift
@@ -21,11 +21,19 @@ final class ParseManager {
       let name = json["name"].stringValue
       let iconString = json["icon"].stringValue
       let store = json["store"].stringValue
+      let releaseDate = json["released_date"].stringValue
+      let updatedDate = json["updated_date"].stringValue
 
       // TODO: delete rating from AppModel. It's not used here.
       let rating = 3.0
 
-      newApp = App(id: id, name: name, iconString: iconString, store: store, rating: rating)
+      newApp = App(id: id,
+                   name: name,
+                   iconString: iconString,
+                   store: store,
+                   rating: rating,
+                   releaseDate: releaseDate,
+                   updatedDate: updatedDate)
 
     } else if let errorMessage = json["message"].string {
 
