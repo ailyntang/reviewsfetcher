415c8b85ed3f0c41abd8f832d56b2c3b1a3bda26
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-14 06:23:46 +1000

    [FF-1] Hide table view when there are no apps to display

--- Source/Home/HomeViewController.swift
+++ Source/Home/HomeViewController.swift
@@ -54,7 +54,7 @@ extension HomeViewController: HomeViewModelDelegate {
   }
 
   func viewModel(_ viewModel: HomeViewModelType, didUpdateIsListAvailableTo isListAvailable: Bool) {
-
+    self.tableView.isHidden = !isListAvailable
   }
 
 }
