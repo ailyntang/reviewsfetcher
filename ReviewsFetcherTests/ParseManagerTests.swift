ee676060095ec2499380ad4b15a241f79a147b93
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-15 07:56:02 +1000

    [FF-1] Test parsing returns an app with parsed parameters when json contains `appId`

--- ReviewsFetcherTests/ParseManagerTests.swift
+++ ReviewsFetcherTests/ParseManagerTests.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+import Quick
+import Nimble
+import SwiftyJSON
+
+@testable import ReviewsFetcher
+
+final class ParseManagerTests: QuickSpec {
+
+  override func spec() {
+
+    describe("Parsing an app overview") {
+
+      // Arrangement
+
+      var app = App()
+
+      let url = Bundle.main.url(forResource: "StubAppOverview", withExtension: "json")
+
+      context("when the json contains an app id") {
+
+        it("should return an app with the parsed parameters") {
+
+          // Action
+
+          do {
+            let data = try Data(contentsOf: url!)
+            let json = JSON(data: data)
+            app = ParseManager.parseAppOverview(from: json)
+          } catch {
+            print(error.localizedDescription)
+          }
+
+          // Assertion
+
+          expect(app.id).to(equal(40332083066))
+          expect(app.name).to(equal("Peoplecare"))
+          expect(app.iconString).to(equal("http://is4.mzstatic.com/image/thumb/Purple117/v4/53/f7/3b/53f73bcc-512a-c205-d53c-a98f86c8cc4f/mzl.uyglrndh.png/100x100bb-85.jpg"))
+          expect(app.store).to(equal("apple"))
+          expect(app.releaseDate).to(equal("2014-10-22T00:00:00"))
+          expect(app.updatedDate).to(equal("2017-05-30T21:46:04"))
+        }
+      }
+
+    }
+  }
+
+}
