51e905bd46d4c171e71a8a674007e43ffdfeda5f
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-16 07:20:15 +1000

    [FF-1] Add placeholder file `NetworkManagerTests`

---
+++ ReviewsFetcherTests/NetworkManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerTests.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 16/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import ReviewsFetcher
+
+final class NetworkManagerTests {
+
+}
