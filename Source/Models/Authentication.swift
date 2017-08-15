17dc00694bf06bcdcdf1195645ddf8dfb52d2ea6
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-16 07:39:13 +1000

    [FF-1] Add authentication model

--- Source/Models/Authentication.swift
+++ Source/Models/Authentication.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+final class Authentication {
+
+  // MARK: Public Properties
+
+  let authValue: String
+
+  let clientKey: String
+
+  // MARK: Constants
+
+  private let username: String
+
+  private let password: String
+
+  // MARK: Initialisation
+
+  init(username: String = "",
+       password: String = "",
+       clientKey: String = "") {
+
+    self.username = username
+    self.password = password
+    self.clientKey = clientKey
+
+    let authCredentials = "\(username):\(password)"
+    let authData = authCredentials.data(using: String.Encoding.utf8)
+    self.authValue = authData!.base64EncodedString(options: NSData.Base64EncodingOptions.lineLength64Characters)
+  }
+
+}
