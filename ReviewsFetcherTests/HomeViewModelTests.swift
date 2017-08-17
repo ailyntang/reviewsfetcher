b90e9d37b7ff91127fda90f2ee633d6bbc92c5a2
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-18 07:38:00 +1000

    [FF-1] Setup placeholders for more `HomeViewModelTests`

--- ReviewsFetcherTests/HomeViewModelTests.swift
+++ ReviewsFetcherTests/HomeViewModelTests.swift
@@ -18,11 +18,12 @@ final class HomeViewModelTests: QuickSpec {
 
     describe("The loadApps method") {
 
+      let viewModelDelegateMock = HomeViewModelDelegateMock()
+
       context("when it has a nil input") {
 
         // Arrangement
         let viewModel = HomeViewModel(with: nil)
-        let viewModelDelegateMock = HomeViewModelDelegateMock()
 
         it("displays the empty state") {
 
@@ -31,14 +32,20 @@ final class HomeViewModelTests: QuickSpec {
           viewModel.delegate?.viewModel(viewModel, didUpdateIsListAvailableTo: false)
 
           // Assertion
-          viewModelDelegateMock.didUpdateIsListAvailableExpectation = self.expectation(description: "didUpdateIsListAvailable should be executed")
+          viewModelDelegateMock.didUpdateIsListAvailableExpectation = self.expectation(description: "didUpdateIsListAvailable should execute")
         }
       }
 
-      context("when it has a non nil input") {
+      context("when it has one app id input") {
 
         // Arrangement
-        let viewModel = HomeViewModel(with: [264519898208, 212243832])
+        let viewModel = HomeViewModel(with: [40332083066])
+        let app = App(id: 40332083066,
+                      name: "Peoplecare",
+                      iconString: "http://is4.mzstatic.com/image/thumb/Purple117/v4/53/f7/3b/53f73bcc-512a-c205-d53c-a98f86c8cc4f/mzl.uyglrndh.png/100x100bb-85.jpg",
+                      store: "apple",
+                      releaseDate: "2014-10-22T00:00:00",
+                      updatedDate: "2017-05-30T21:46:04")
 
         it("makes a network call") {
 
@@ -49,13 +56,29 @@ final class HomeViewModelTests: QuickSpec {
         }
 
         it("displays a list of apps when the call is successful") {
+
+          // Action
           viewModel.delegate?.viewModel(viewModel, didUpdateIsListAvailableTo: true)
+
+          // Assertion
+          viewModelDelegateMock.didUpdateIsListAvailableExpectation = self.expectation(description: "didUpdateIsListAvailable should execute")
         }
+
+
+
+        it("updates the app overview with the fetched app") {
+
+          // Action
+          viewModel.delegate?.viewModel(viewModel, didUpdateAppOverviewTo: app)
+        }
+
+      }
+
+      context("when it has more than one app id input") {
 
         it("sorts the apps in alphabetical order") {
 
         }
-
       }
     }
   }
@@ -69,8 +92,14 @@ private final class HomeViewModelDelegateMock: HomeViewModelDelegate {
 
   var isListAvailable: Bool?
 
-  func viewModel(_ viewModel: HomeViewModelType, didUpdateAppOverviewTo app: App) {
+  var app: App?
 
+  var didUpdateAppOverviewExpectation: XCTestExpectation?
+
+  func viewModel(_ viewModel: HomeViewModelType, didUpdateAppOverviewTo app: App) {
+    self.app = app
+
+    didUpdateAppOverviewExpectation.fulfill()
   }
 
   func viewModel(_ viewModel: HomeViewModelType, didSortListBy appProperty: String) {
