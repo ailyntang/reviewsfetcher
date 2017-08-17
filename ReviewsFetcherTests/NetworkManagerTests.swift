333a62aaa46b8b816bbc09691820cface4e0887f
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-17 21:09:56 +1000

    [FF-1] Test network manager response with successful authentication

--- ReviewsFetcherTests/NetworkManagerTests.swift
+++ ReviewsFetcherTests/NetworkManagerTests.swift
@@ -18,10 +18,35 @@ final class NetworkManagerTests: QuickSpec {
 
     describe("The network manager") {
 
+      // Arrangement
+      var app = App()
+
+      let auth = AuthenticationSecrets()
+      let authSuccess = Authentication(username: auth.username,
+                                       password: auth.password,
+                                       clientKey: auth.clientKey)
+      let authFailure = Authentication()
+
       context("when authentication succeeeds") {
 
         it("should fetch an app overview") {
 
+          waitUntil(timeout: 4) { done in
+
+            // Action
+            NetworkManager.fetchAppOverview(auth: authSuccess, appId: 40332083066, completionHandler: { newApp in
+              app = newApp
+
+              // Assertion
+              expect(app.name).to(equal("Peoplecare"))
+              expect(app.id).to(equal(40332083066))
+              expect(app.iconString).to(equal("http://is1.mzstatic.com/image/thumb/Purple127/v4/aa/27/95/aa2795b2-add6-6ece-2ba6-035d7900f60c/source/1280x1280bb.png"))
+              expect(app.releaseDate).to(equal("2014-10-22T00:00:00"))
+              expect(app.updatedDate).toNot(beNil())
+
+              done()
+            })
+          }
         }
       }
 
