dee1f5112531e1ab83699bb06733eed104fc7e73
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-20 13:33:12 +1000

    [FF-1] Update parse manager tests as parse manager returns `App?` instead of `App`

--- ReviewsFetcherTests/ParseManagerTests.swift
+++ ReviewsFetcherTests/ParseManagerTests.swift
@@ -20,8 +20,7 @@ final class ParseManagerTests: QuickSpec {
     describe("Parsing an app overview") {
 
       // Arrangement
-
-      var app = App()
+      var app: App?
 
       let urlAppOverview = Bundle.main.url(forResource: "StubAppOverview", withExtension: "json")
       let urlMessage = Bundle.main.url(forResource: "StubAppFiguresMessage", withExtension: "json")
@@ -32,7 +31,6 @@ final class ParseManagerTests: QuickSpec {
         it("should return an app with the parsed parameters") {
 
           // Action
-
           do {
             let data = try Data(contentsOf: urlAppOverview!)
             let json = JSON(data: data)
@@ -42,22 +40,24 @@ final class ParseManagerTests: QuickSpec {
           }
 
           // Assertion
+          expect(app).toNot(beNil())
 
-          expect(app.id).to(equal(40332083066))
-          expect(app.name).to(equal("Peoplecare"))
-          expect(app.iconString).to(equal("http://is4.mzstatic.com/image/thumb/Purple117/v4/53/f7/3b/53f73bcc-512a-c205-d53c-a98f86c8cc4f/mzl.uyglrndh.png/100x100bb-85.jpg"))
-          expect(app.store).to(equal("apple"))
-          expect(app.releaseDate).to(equal("2014-10-22T00:00:00"))
-          expect(app.updatedDate).to(equal("2017-05-30T21:46:04"))
+          if let app = app {
+            expect(app.id).to(equal(40332083066))
+            expect(app.name).to(equal("Peoplecare"))
+            expect(app.iconString).to(equal("http://is4.mzstatic.com/image/thumb/Purple117/v4/53/f7/3b/53f73bcc-512a-c205-d53c-a98f86c8cc4f/mzl.uyglrndh.png/100x100bb-85.jpg"))
+            expect(app.store).to(equal("apple"))
+            expect(app.releaseDate).to(equal("2014-10-22T00:00:00"))
+            expect(app.updatedDate).to(equal("2017-05-30T21:46:04"))
+          }
         }
       }
 
       context("when the json contains a message") {
 
-        it("should return an app with default parameters") {
+        it("should return nil") {
 
           // Action
-
           do {
             let data = try Data(contentsOf: urlMessage!)
             let json = JSON(data: data)
@@ -67,21 +67,15 @@ final class ParseManagerTests: QuickSpec {
           }
 
           // Assertion
-
-          expect(app.name).to(equal("Error: app not found"))
-          expect(app.iconString).to(beNil())
-          expect(app.store).to(equal("n/a"))
-          expect(app.releaseDate).to(equal("1900-01-01T00:00:00"))
-          expect(app.updatedDate).to(equal("1900-01-01T00:00:00"))
+          expect(app).to(beNil())
         }
       }
 
       context("when the json has no app id and no message") {
 
-        it("should return an app with default parameters") {
+        it("should return nil") {
 
           // Action
-
           do {
             let data = try Data(contentsOf: urlUnexpected!)
             let json = JSON(data: data)
@@ -91,12 +85,7 @@ final class ParseManagerTests: QuickSpec {
           }
 
           // Assertion
-
-          expect(app.name).to(equal("Unknown error from App Figures API"))
-          expect(app.iconString).to(beNil())
-          expect(app.store).to(equal("n/a"))
-          expect(app.releaseDate).to(equal("1900-01-01T00:00:00"))
-          expect(app.updatedDate).to(equal("1900-01-01T00:00:00"))
+          expect(app).to(beNil())
         }
       }
     }
