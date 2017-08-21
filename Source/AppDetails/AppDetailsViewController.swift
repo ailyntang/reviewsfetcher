d70c32a320f9ca657478bd2ec5cd85c81111ac97
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-22 06:44:51 +1000

    [FF-2] Add view controller for app details scene

---
+++ Source/AppDetails/AppDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AppDetailsViewController.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 22/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+final class AppDetailsViewController: UIViewController {
+
+}
