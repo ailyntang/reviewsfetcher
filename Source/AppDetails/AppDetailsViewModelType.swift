da3c9b38ba245fffe5732368815a77c51b907003
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-23 07:04:46 +1000

    [FF-2] Add placeholder `AppDetailsViewModelType`

---
+++ Source/AppDetails/AppDetailsViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  AppDetailsViewModelType.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 23/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
+
+final class AppDetailsViewModelType {
+
+}
