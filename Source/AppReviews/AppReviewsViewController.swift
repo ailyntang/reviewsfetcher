e512c985f6d1df04380fd9ebdfc3770c85627a3e
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-24 07:13:09 +1000

    [FF-2] Add `MARK` to `AppReviewsViewController to structure the file

--- Source/AppReviews/AppReviewsViewController.swift
+++ Source/AppReviews/AppReviewsViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 final class AppReviewsViewController: UIViewController {
 
-  @IBOutlet weak var label: UILabel!
+  // MARK: IBOutlets
 
+  @IBOutlet fileprivate weak var label: UILabel!
+
+  // MARK: Public Properties
+
+  // TODO: how can we make this private? Think about using a delegate for the segue?
   var receivedAppId: Int?
+
+  // MARK: Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     label.text = String(describing: receivedAppId)
   }
+
+
 }
