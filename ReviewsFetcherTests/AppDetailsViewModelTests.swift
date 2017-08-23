73ed84fd86baad0e88052b7eb8d403fead4bcc23
Ai-Lyn Tang <ailyn.tang@outware.com.au>
2017-08-24 06:34:25 +1000

    [FF-2] Add new placeholder test file for app details

---
+++ ReviewsFetcherTests/AppDetailsViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  AppDetailsViewModelTests.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 24/8/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import Foundation
