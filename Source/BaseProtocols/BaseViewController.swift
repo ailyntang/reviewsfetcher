0a0609e1835b37aa214792ec00d9f60113bc21d4
Ai-Lyn Tang <ailyn.tang@auspost.com.au>
2017-08-27 19:16:29 +1000

    [FF-2] Move `setupActivityIndicator` to `BaseViewController`
    This method is used by all view controllers.
    Makes sense that we move it to the base VC so we don't duplicate it for each VC.

--- Source/BaseProtocols/BaseViewController.swift
+++ Source/BaseProtocols/BaseViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+
+  // MARK: Properties
+
+  fileprivate var activityIndicator = UIActivityIndicatorView()
+
+  // MARK: Public Methods
+
+  func setupActivityIndicator(state: String) {
+    activityIndicator.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: 40, height: 40))
+    activityIndicator.center = self.view.center
+    activityIndicator.activityIndicatorViewStyle = .gray
+    view.addSubview(activityIndicator)
+
+    if state == "start" {
+      activityIndicator.startAnimating()
+    } else {
+      activityIndicator.stopAnimating()
+    }
+  }
+  
+}
