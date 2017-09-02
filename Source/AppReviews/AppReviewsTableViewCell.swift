75af416570ba511bf0b56cd86f4b48764465dbe8
Ai-Lyn Tang <ailyntang@gmail.com>
2017-09-02 19:03:19 +1000

    [FF-2] Display `AppReview` data in the table cell

--- Source/AppReviews/AppReviewsTableViewCell.swift
+++ Source/AppReviews/AppReviewsTableViewCell.swift
@@ -17,10 +17,32 @@ final class AppReviewsTableViewCell: UITableViewCell {
   @IBOutlet weak private var titleLabel: UILabel!
   @IBOutlet weak private var reviewLabel: UILabel!
 
+  // MARK: Properties
+
+  private var dateFormatter = DateFormatter()
+
   // MARK: Public Methods
 
   func setupAppReview(with review: AppReview) {
+    starLabel.text = "\(review.stars) stars"
 
+    // Read date string from `AppReview` model
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    let reviewDate = dateFormatter.date(from: review.date)
+
+    // Transform date into desired format
+    dateFormatter.dateFormat = "dd MMM yyyy"
+    let reviewDateString = dateFormatter.string(from: reviewDate!)
+
+    dateAndVersionLabel.text = "\(reviewDateString) (\(review.version))"
+
+    if let title = review.title {
+      titleLabel.text = title
+    }
+
+    if let review = review.review {
+      reviewLabel.text = review
+    }
   }
 
 }
