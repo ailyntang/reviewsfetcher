e7e96f93d9cf9ef019315aaeaffab593affbd769
Ai-Lyn Tang <ailyntang@gmail.com>
2017-09-10 19:19:25 +1000

    [FF-2-3b] Add new `StarRating.swift` file

---
+++ Source/Models/StarRating.swift
@@ -0,0 +1,13 @@
+//
+//  StarRating.swift
+//  ReviewsFetcher
+//
+//  Created by Ai-Lyn Tang on 10/9/17.
+//  Copyright © 2017 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+final class StarRating: UIStackView {
+  
+}
